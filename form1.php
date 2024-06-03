90a251bfeac69fa9c0adf3970a3d182a1730c17d
mandadivishnuvardhanreddy <107764020+mandadivishnuvardhanreddy@users.noreply.github.com>
2024-06-03 13:45:19 +0530

    Update form1.php

--- form1.php
+++ form1.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="style1.css">  
@@ -9,16 +8,22 @@
      
       if($_SERVER['REQUEST_METHOD']=="POST")
       { 
-        $name = $_POST['Agency'];
+        $name = $_POST['name'];
         $email = $_POST['email'];
-        $phone=$_POST['phone'];
-
-        
-      
+        $hostelname = $_POST['h1name'];
+        $boys = isset($_POST['boys']) ? 1 : 0;
+        $girls = isset($_POST['girls']) ? 1 : 0;
+        $rooms1 = isset($_POST['1bhk']) ? 1 : 0;
+        $rooms2 = isset($_POST['2bhk']) ? 1 : 0;
+        $rooms3 = isset($_POST['3bhk']) ? 1 : 0;
+        $rooms4 = isset($_POST['4bhk']) ? 1 : 0;
+        $minPrice = $_POST['quantity1'];
+        $collegename = $_POST['hname'];
+        $maxPrice = $_POST['quantity2'];
         $severname="localhost";
         $username="root";
         $password="";
-        $database="maps";
+        $database="hostel128";
         $conn=mysqli_connect($severname,$username,$password,$database);
   
         if(!$conn)
@@ -29,61 +34,42 @@
          echo "connected";
         }
         
-        $sql = "INSERT INTO `register` (`Name of Agency`,`Phone`,`Email`,`longitude`,`latitude` ) VALUES ('name','email',phone)";
+        $sql = "INSERT INTO `hostel133` (`name`, `email`, `hostelname`, `boys`, `girls`, `rooms1`, `rooms2`, `rooms3`, `rooms4`, `collegename`, `minPrice`, `maxPrice`) VALUES ('$name', '$email', '$hostelname', $boys, $girls, $rooms1, $rooms2, $rooms3, $rooms4, '$collegename', $minPrice, $maxPrice)";
         mysqli_query($conn,$sql);
         echo "inserted";
       
     }
     ?>
-  
-        <form action="form1.php">
-            <div class="c1">
-                <h1>Register</h1>
-                <hr>
-                <label for="text"><b>Name of Agency</b>
-                </label>
-                <input type="text" placeholder="Enter name" name="Agency" id="ag" required>
-                <label for="email"><b>Email</b></label>
-                <input type="text" placeholder="Enter email" name="email" id="email" required>
-                <label for="psw"><b>Password</b></label>
-              
-                <input type="number" placeholder="Enter phone" name="phone" id="phone" required>
-                <label for="addr"><b>Address</b></label>
+        <h1  class="head">JOIN US</h1>
+        <h1 class="head">UPGRADE YOUR BUSINESS</h1>
+        <div class="containe" >
+            <div class="container" >
+            <form action='post.php' method="post">
+                <h2>Login</h2>
+                <input type="name" required placeholder="name" name="name" />
+                <input type="name" required placeholder="Email Address" name="email"/>
                 
-                <input type="checkbox" id="e1" name="e1" value="Floods">
-                <label for="e1">Floods</label><br>
-                <input type="checkbox" id="e2" name="e2" value="Earthquake">
-                <label for="e1">Earthquakes</label><br>
-                <input type="checkbox" id="e3" name="e3" value="Fire accidents">
-                <label for="e1">Fire accidents</label><br>
-                <p>Click the button to get your coordinates.</p>
-
-<button onclick="getLocation()">Try It</button>
-
-<p id="demo"></p>
-
-<script>
-var x = document.getElementById("demo");
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
-}
-</script>
-
-                <button type="submit" class="sub">Register</button>
-            </div>
-            <div class="c2">
-                <p>Already have an accont?<a href="#">Sign in</a></p>
-            </div>
-        </form>
+                <h4>Enter Hostel name</h4>
+                <input type="text" id="h1name" name="h1name"><br>
+                <h4>Accomdation for</h4>
+                <input type="checkbox" name="boys" value="boys">Boys<br>
+        <input type="checkbox" name="girls" value="boys">Girls<br>
+                <h4>types of rooms available</h4>
+                <input type="checkbox" name="1bhk"  class="checkbox" value="1 sharing">1 sharing<br>
+        <input type="checkbox" name="2bhk"class="checkbox"  value="2 sharing">2 sharing<br>
+        <input type="checkbox" name="3bhk" class="checkbox"  value="3 sharing">3 sharing<br>
+        <input type="checkbox" name="4bhk"class="checkbox"  value="4 sharing">4 sharing<br>
+        <h4>Enter nearby college</h4>
+        <input type="text" id="hname" name="hname"><br>
+        <h4>Hostel price</h4>
+        <input type="number" id="min" name="quantity1" min="1" max="100000">
+        <input type="number" id="max" name="quantity2" min="1" max="100000">
+                <button>Login</button>
+            </form>
+        </div>
+        </div>
     </body>
 </html>
+
+
+
